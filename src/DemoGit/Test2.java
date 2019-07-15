ae8c8287d87871c2c04c4927011a2d2d7af23293
vivek.tiwari <vivek.tiwari@Vivek-Tiwari1.diasparkindia.com>
2019-07-15 18:31:08 +0530

    Committing Test2 to BranchX

---
+++ src/DemoGit/Test2.java
@@ -0,0 +1,15 @@
+package DemoGit;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		String hungry = "yes";
+		if (hungry == "Yes")
+			System.out.println("eat Something");
+		else {
+			System.out.println("Do Work...");
+		}
+
+	}
+
+}
